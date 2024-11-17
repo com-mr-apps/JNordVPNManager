55a5f6323117e5ccd5b910d68bd5c070d1ebabc2
mr <186610986+com-mr-apps@users.noreply.github.com>
2024-11-17 17:47:59 +0200

    Update version to 2024.2.1

--- createSnap.sh
+++ createSnap.sh
@@ -6,3 +6,5 @@ snapcraft clean #force build from GitHub
 snapcraft
 
 sudo snap install j-nordvpn-manager_2024.1.1_amd64.snap --devmode --dangerous
+
+snapcraft upload --release=stable j-nordvpn-manager_2024.1.1_amd64.snap
